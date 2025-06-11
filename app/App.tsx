ee0f9b3a68de1468131d17f0278b3036c2c5da67
yakiv <yakov.manitskiy@gmail.com>
2025-06-11 20:43:48 +0300

    add FlatList for posts rendering, add loading

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StatusBar, Text, View, Alert} from 'react-native';
+import {StatusBar, Text, View, Alert, FlatList, ActivityIndicator} from 'react-native';
 import '../global.css';
 import {Post} from "@/components/Post";
 import axios from "axios";
@@ -12,12 +12,17 @@ type Posts = {
 
 export default function App() {
   const [posts, setPosts] = React.useState<Posts[]>([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
+    setLoading(true);
+
     const fetchPosts = async () => {
       try {
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
         setPosts(response.data);
+
+        setLoading(false);
       } catch (error) {
         console.log(error);
         Alert.alert('Posts not found', 'Please try again later.');
@@ -27,17 +32,26 @@ export default function App() {
     fetchPosts();
   }, []);
 
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#0000ff" animating={loading} />
+      </View>
+    )
+  }
+
   return (
     <View className="mx-4">
       <StatusBar barStyle="default" />
 
-      {/* Posts wrapper */}
       <Text className="text-xl font-semibold mb-6">Posts</Text>
-      {
-        posts.map((post) => (
-          <Post key={post.id} {...post} />
-        ))
-      }
+
+      <FlatList
+        data={posts}
+        renderItem={({item}) => <Post {...item} />}
+        keyExtractor={item => item.id.toString()}
+      />
+
     </View>
   );
 }
